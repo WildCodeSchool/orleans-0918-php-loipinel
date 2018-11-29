bb37e554ce4425597454958e370918ae180102d2
Christophe LESIEUR <chrlesieur@free.fr>
2018-11-29 15:00:35 +0100

    modif executed

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
     .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
-
+    .addEntry('requestApi', './assets/js/apiAdressRequest.js')
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
