b5c5bfe3c5359bf74eae8e41960ddda2ac6f556d
Christophe LESIEUR <chrlesieur@free.fr>
2018-11-30 16:05:25 +0100

    nouvelle modif js

--- assets/js/apiAdressRequest.js
+++ assets/js/apiAdressRequest.js
@@ -4,18 +4,18 @@ zipcode.addEventListener('input', function (e) {
     $('#simulator_city').empty();
 
     if (this.value.length === 5) {
-        fetch("https://api-adresse.data.gouv.fr/search/?q=postcode==" + postalCode + "&limit=100")
+        fetch("https://api-adresse.data.gouv.fr/search/?q=" + postalCode + "&limit=100")
             .then((resp) => resp.json())
-            .then((data) => fillSelector(data.features))
+            .then((data) => fillSelector(data.features, postalCode))
     }
 });
 
-function fillSelector(data) {
+function fillSelector(data, postalCode) {
 
     let selectElt = document.getElementById('simulator_city');
     let cities=[];
     for (cityData of data) {
-        if (!cities.hasOwnProperty(cityData.properties.citycode)){
+        if (!cities.hasOwnProperty(cityData.properties.citycode) && (cityData.properties.citycode !== postalCode)){
             cities[cityData.properties.citycode] = cityData.properties.city;
             let newOptionElt = document.createElement('option');
             newOptionElt.innerText = cityData.properties.postcode + ' ' + cityData.properties.city;
