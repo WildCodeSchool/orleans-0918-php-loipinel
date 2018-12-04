3cee55fb08db641c4cb402dc94f6ddc0b37a6221
Rémy DUCRY <ducry.remy@gmail.com>
2018-12-04 14:43:50 +0100

    renommage variables dans le service CalculationTaxBenefit pour une meilleure compréhension avec ajout PHPDOC

--- src/Service/CalculationTaxBenefit.php
+++ src/Service/CalculationTaxBenefit.php
@@ -10,18 +10,28 @@ namespace App\Service;
 
 class CalculationTaxBenefit
 {
-    public function taxBenefit($taxBase, $time)
+    const MAXIMUM_TAX_BASE = 300000;
+    const RATE_FOR_A_PERIOD_OF_SIX_YEARS = 0.12;
+    const RATE_FOR_A_PERIOD_OF_NINE_YEARS = 0.18;
+    const RATE_FOR_A_PERIOD_OF_TWELVE_YEARS = 0.21;
+
+    /**
+     * @param int $taxBase
+     * @param int $time
+     * @return int
+     */
+    public function taxBenefit(int $taxBase, int $time) : int
     {
         $taxBenefit = 0;
-        if ($taxBase > 300000) {
+        if ($taxBase > self::MAXIMUM_TAX_BASE) {
             $taxBase = 300000;
         }
         if ($time == 6) {
-            $taxBenefit = $taxBase * 0.12;
+            $taxBenefit = $taxBase * self::RATE_FOR_A_PERIOD_OF_SIX_YEARS;
         } elseif ($time == 9) {
-            $taxBenefit = $taxBase * 0.18;
+            $taxBenefit = $taxBase * self::RATE_FOR_A_PERIOD_OF_NINE_YEARS;
         } else {
-            $taxBenefit = $taxBase * 0.21;
+            $taxBenefit = $taxBase * self::RATE_FOR_A_PERIOD_OF_TWELVE_YEARS;
         }
         return $taxBenefit;
     }
