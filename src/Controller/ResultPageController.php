d4e9b9178f236582447e4e28e8d6effe15081d60
Rémy DUCRY <ducry.remy@gmail.com>
2018-12-04 11:13:37 +0100

    ajout tests unitaires pour le calcul de la base fiscale, modification du controller de résultat, modification de la vue

--- src/Controller/ResultPageController.php
+++ src/Controller/ResultPageController.php
@@ -5,17 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\TaxBase;
+use App\Service\CalculationTaxBenefit;
 
 class ResultPageController extends AbstractController
 {
     /**
      * @Route("/resultat", name="result_page")
      */
-    public function index(SessionInterface $session)
+    public function index(SessionInterface $session, TaxBase $taxBase, CalculationTaxBenefit $calculationTaxBenefit)
     {
         $simulator = $session->get('simulator');
+        $base = $taxBase->taxBase($simulator->getPurchasePrice(), $simulator->getSurfaceArea());
+        $result = $calculationTaxBenefit->taxBenefit($base, $simulator->getDuration());
+
         return $this->render('result.html.twig', [
             'simulator' => $simulator,
+            'result' => $result,
         ]);
     }
 }
---
+++ tests/TaxBaseTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 04/12/18
+ * Time: 11:05
+ */
+
+namespace Tests;
+
+use App\Service\TaxBase;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class TaxBaseTest extends TestCase
+{
+    public function testBaseUnderMax()
+    {
+        $op = new TaxBase();
+        $this->assertEquals(400000, $op->taxBase(400000, 100));
+        $this->assertEquals(210000, $op->taxBase(210000, 70));
+        $this->assertEquals(250000, $op->taxBase(250000, 100));
+    }
+    public function testBaseOverMax()
+    {
+        $op = new TaxBase();
+        $this->assertEquals(550000, $op->taxBase(1000000, 100));
+        $this->assertEquals(412500, $op->taxBase(487500, 75));
+        $this->assertEquals(275000, $op->taxBase(350000, 50));
+    }
+}
