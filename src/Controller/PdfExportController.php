f621903298e00c42ee352e58240de82a53f69fdb
Thomas Coumes <thomascoumes3145@gmail.com>
2018-12-05 09:57:25 +0100

    pdf filename with last name + date

--- src/Controller/PdfExportController.php
+++ src/Controller/PdfExportController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
@@ -15,16 +14,18 @@ class PdfExportController extends AbstractController
      * @Route("/export-pdf", name="pdf_export")
      * @return PdfResponse
      */
-    public function pdfAction(Pdf $knpSnappyPdf)
+    public function pdfAction(Pdf $knpSnappyPdf, SessionInterface $session)
     {
         /* creating the pdf from html page */
         $html = $this->renderView('resume.html.twig');
+        $simulator = $session->get('simulator');
+        $lastName = $simulator->getLastName();
 
         return new PdfResponse(
             $knpSnappyPdf->getOutputFromHtml($html, array(
                 'encoding' => 'utf-8',
             )),
-            'simulation.pdf'
+            $lastName . '-' . date("d_m_Y") . '.pdf'
         );
     }
 }
