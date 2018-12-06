358b7750d21a520ea54b3c6d9eee41028a84a2f3
Anthony Jonas <anthony.jonas@outlook.com>
2018-12-06 16:03:57 +0100

    Suppression appel user dans SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,7 +14,6 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $user = $this->getUser();
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
