3d0a67918dc89121b9aa8a6cdc693783a5497de5
Rémy DUCRY <ducry.remy@gmail.com>
2018-12-10 11:33:57 +0100

    Ajout de la méthode pour le calcul de l'avantage fiscal

--- src/Service/TaxBenefit.php
+++ src/Service/TaxBenefit.php
@@ -7,12 +7,22 @@ use App\Entity\RealEstateProperty;
 class TaxBenefit
 {
     const MAXIMUM_PRICE_PER_SQUARE_METER = 5500;
+    const MAXIMUM_TAX_BASE = 300000;
+    const RATE_FOR_A_PERIOD_OF_SIX_YEARS = 0.12;
+    const RATE_FOR_A_PERIOD_OF_NINE_YEARS = 0.18;
+    const RATE_FOR_A_PERIOD_OF_TWELVE_YEARS = 0.21;
 
     /**
      * @var RealEstateProperty
      */
     private $realEstate;
 
+    /**
+     * Durée de la location en année
+     *
+     * @var int
+     */
+    private $rentalPeriod;
 
     /**
      * Calcule la base fiscale du bien
@@ -26,6 +36,55 @@ class TaxBenefit
             $meterSquarePrice = self::MAXIMUM_PRICE_PER_SQUARE_METER;
         }
         return $meterSquarePrice * $this->realEstate->getSurfaceArea();
+    }
+
+    /**
+     * Calcule l'avantage fiscal en se basant sur la base fiscale et la durée
+     *
+     * @return int
+     */
+    public function calculateTaxBenefit() : int
+    {
+        $taxBenefit = 0;
+
+        if ($this->calculateTaxBase() > self::MAXIMUM_TAX_BASE) {
+            $taxBase = self::MAXIMUM_TAX_BASE;
+        }
+
+        switch ($this->getRentalPeriod()) {
+            case 6:
+                $taxBenefit = $taxBase * self::RATE_FOR_A_PERIOD_OF_SIX_YEARS;
+                break;
+            case 9:
+                $taxBenefit = $taxBase * self::RATE_FOR_A_PERIOD_OF_NINE_YEARS;
+                break;
+            case 12:
+                $taxBenefit = $taxBase * self::RATE_FOR_A_PERIOD_OF_TWELVE_YEARS;
+                break;
+            default:
+                throw new \LogicException("Only 6, 9, 12 accepted.");
+                break;
+        }
+
+        return $taxBenefit;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRentalPeriod(): int
+    {
+        return $this->rentalPeriod;
+    }
+
+    /**
+     * @param int $rentalPeriod
+     * @return TaxBenefit
+     */
+    public function setRentalPeriod(int $rentalPeriod): TaxBenefit
+    {
+        $this->rentalPeriod = $rentalPeriod;
+        return $this;
     }
 
     /**
