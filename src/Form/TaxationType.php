0823c7402ce681ad98039667f3fc93bf82233a86
Rémy DUCRY <ducry.remy@gmail.com>
2019-01-03 17:56:29 +0100

    suppression use inutile dans TaxationType

--- src/Form/TaxationType.php
+++ src/Form/TaxationType.php
@@ -14,7 +14,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class TaxationType extends AbstractType
