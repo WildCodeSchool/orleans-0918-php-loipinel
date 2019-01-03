b60b2642f446b45ded4343104ddfc65f5f6ca7f2
Jovanela Daribo <carly972@live.fr>
2019-01-03 23:07:54 +0100

    Correction erreur javascript sur code fermeture modal

--- assets/js/requestArea.js
+++ assets/js/requestArea.js
@@ -1,18 +1,18 @@
 let acquisitionDate = document.getElementById('finance_acquisitionDate');
 let city = document.getElementById('finance_city');
 
-function request(){
-    if(acquisitionDate.value && city.value ){
+function request() {
+    if (acquisitionDate.value && city.value) {
         let date = $('#finance_acquisitionDate').val();
         let cityCode = $('#finance_city').val();
         $('#finance_zone').empty();
-        fetch("/area/"+ date +"/"+ cityCode)
+        fetch("/area/" + date + "/" + cityCode)
             .then((resp) => resp.json())
-            .then((data)=>setArea(data))
+            .then((data) => setArea(data))
     }
 }
 
-acquisitionDate.addEventListener('input', function (e){
+acquisitionDate.addEventListener('input', function (e) {
     request();
 });
 
@@ -23,11 +23,9 @@ city.addEventListener('change', function (e) {
 function setArea(area) {
     let selectElt = document.getElementById('finance_zone');
     selectElt.value = area;
-    if(area === 'C') {
+    if (area === 'C') {
         modal.style.display = 'block';
-        $(function () {
-            $('#modal').modal().close();
-        });
+        $('#modal').modal().close();
     }
 }
 
