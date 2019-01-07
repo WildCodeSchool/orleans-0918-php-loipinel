a2917fbe9b5931bea48d0533a0ee6bbb9e866c62
Thomas Coumes <thomascoumes3145@gmail.com>
2019-01-07 11:59:07 +0100

    french label on variable edit

--- src/Form/VariableType.php
+++ src/Form/VariableType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Variable;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,11 @@ class VariableType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('maximumPricePerSquareMeter')
-            ->add('maximumTaxBase')
-            ->add('rateFor6Years')
-            ->add('rateFor9Years')
-            ->add('rateFor12Years')
+            ->add('maximumPricePerSquareMeter', IntegerType::class, ['label' => 'Prix maximum par mètre carré'])
+            ->add('maximumTaxBase', IntegerType::class, ['label' => 'Base fiscale maximum'])
+            ->add('rateFor6Years', NumberType::class, ['label' => 'Taux pour 6 ans'])
+            ->add('rateFor9Years', NumberType::class, ['label' => 'Taux pour 9 ans'])
+            ->add('rateFor12Years', NumberType::class, ['label' => 'Taux pour 12 ans'])
         ;
     }
 
