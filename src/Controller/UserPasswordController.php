f7be8bb266b0039531046b59060a6e6f023af09d
Thomas Coumes <thomascoumes3145@gmail.com>
2019-01-08 14:17:53 +0100

    fix 5xx error

--- src/Controller/UserPasswordController.php
+++ src/Controller/UserPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
