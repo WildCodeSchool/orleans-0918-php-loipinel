c818cfaf83b18e8cea3a0c934f6681e95a6c1935
Thomas Coumes <thomascoumes3145@gmail.com>
2019-01-09 17:07:32 +0100

    add submit button

--- src/Form/ProfileSelfUpdateType.php
+++ src/Form/ProfileSelfUpdateType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -26,7 +27,12 @@ class ProfileSelfUpdateType extends AbstractType
                 'required' => false,
                 'label' => 'Nouveau prénom',
             ])
-        ;
+            ->add('changePassword', SubmitType::class, [
+                'label' => 'Editer mes informations',
+                'attr'  => [
+                    'class' => 'btn btn-primary text-align-center',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
