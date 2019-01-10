7ae16a556e2466a4233303601da2427eb3f21476
Thomas Coumes <thomascoumes3145@gmail.com>
2019-01-10 11:24:30 +0100

    resolving travis problem

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -168,4 +168,37 @@ class User implements UserInterface
     {
         $this->firstName = $firstName;
     }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
 }
